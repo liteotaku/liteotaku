a34cfe4c7f7bcabe6c9b406a13f44a9cadafeef2
m.vladimirov <mprog54@gmail.com>
2021-04-22 08:46:04 +0700

    Updated site.

--- build.bat
+++ build.bat
@@ -1,5 +1,8 @@
+rem hikaru clean --debug & hikaru build --debug & git add --all & git commit --message "Updated site." & git push --set-upstream origin master
+cd C:\Users\Admin\Documents\WEB\blog-test
 hikaru clean --debug
 hikaru build --debug
 git add --all
 git commit --message "Updated site."
 git push --set-upstream origin master
+pause
